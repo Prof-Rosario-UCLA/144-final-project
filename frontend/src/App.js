7490aee9a2d567c680715b95045cc4b398781ea3
Bach Ngo <ngo.b48@gmail.com>
2025-05-14 18:40:37 -0700

    init React app w/ Tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div
+    className="flex w-screen h-screen bg-blue-800"
+    >
+      <h1
+      className="text-white font-2xl bg-red-900 m-auto"
+      >
+        Final Project
+      </h1>
+    </div>
+  );
+}
+
+export default App;
